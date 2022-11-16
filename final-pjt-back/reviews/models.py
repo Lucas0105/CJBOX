1b4ff24e2a849e12ac62967c4f0406598bfc9b48
Lucas0105 <dnjswo105@gmail.com>
2022-11-16 17:17:40 +0900

    [add]: comment model

--- final-pjt-back/reviews/models.py
+++ final-pjt-back/reviews/models.py
@@ -5,6 +5,14 @@ from django.conf import settings
 class Review(models.Model):
     movie = models.ForeignKey('movies.Movie', on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    comments = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='comments', through='Comment')
     content = models.TextField()
     vote = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
