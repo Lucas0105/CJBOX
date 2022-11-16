f3c3f94bd3883fe95597e281943e02468e61ec85
Lucas0105 <dnjswo105@gmail.com>
2022-11-16 17:09:03 +0900

    [add]: genre table

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.conf import settings
 
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+
 # Create your models here.
 class Movie(models.Model):
+    genres = models.ManyToManyField(Genre, related_name='movies')
     reviews = models.ManyToManyField(settings.AUTH_USER_MODEL, through='reviews.Review', related_name='reviews')
     movie_id = models.IntegerField()
     poster_path = models.CharField(max_length=100)
