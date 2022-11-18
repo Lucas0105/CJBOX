716db170131ecdc73ad1d2f125f112ed7f338c48
Lucas0105 <dnjswo105@gmail.com>
2022-11-18 09:13:06 +0900

    [Test]: swiper

--- final-pjt-front/src/components/BannerList.vue
+++ final-pjt-front/src/components/BannerList.vue
@@ -1,17 +1,205 @@
 <template>
-  <div>
-    <h1>BannerList</h1>
-  </div>
+  <swiper
+    slidesPerView="auto"
+    :scrollbar="{ draggable: true }"
+    mousewheel
+    freeMode
+    navigation
+  >
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--4NgMf3RF--/v1521804358/avengers.jpg"
+        title="Avengers : Infinity War"
+        description="Thanos is around"
+        duration="2hrs"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--BmgguRnX--/v1521804402/thor.jpg"
+        title="Thor : Ragnarok"
+        description="Thor lost his hair"
+        duration="2hrs30mins"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--qXaW5V3E--/v1521804426/wakanda.jpg"
+        title="Black Panther"
+        description="Wakanda Forever"
+        duration="2hrs15mins"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--4NgMf3RF--/v1521804358/avengers.jpg "
+        title="Avengers : Infinity War"
+        description="Thanos is around"
+        duration="2hrs"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--BmgguRnX--/v1521804402/thor.jpg"
+        title="Thor : Ragnarok"
+        description="Thor lost his hair"
+        duration="2hrs30mins"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--qXaW5V3E--/v1521804426/wakanda.jpg"
+        title="Black Panther"
+        description="Wakanda Forever"
+        duration="2hrs15mins"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--4NgMf3RF--/v1521804358/avengers.jpg "
+        title="Avengers : Infinity War"
+        description="Thanos is around"
+        duration="2hrs"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--BmgguRnX--/v1521804402/thor.jpg"
+        title="Thor : Ragnarok"
+        description="Thor lost his hair"
+        duration="2hrs30mins"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--qXaW5V3E--/v1521804426/wakanda.jpg"
+        title="Black Panther"
+        description="Wakanda Forever"
+        duration="2hrs15mins"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--4NgMf3RF--/v1521804358/avengers.jpg"
+        title="Avengers : Infinity War"
+        description="Thanos is around"
+        duration="2hrs"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--BmgguRnX--/v1521804402/thor.jpg"
+        title="Thor : Ragnarok"
+        description="Thor lost his hair"
+        duration="2hrs30mins"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--qXaW5V3E--/v1521804426/wakanda.jpg"
+        title="Black Panther"
+        description="Wakanda Forever"
+        duration="2hrs15mins"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--4NgMf3RF--/v1521804358/avengers.jpg "
+        title="Avengers : Infinity War"
+        description="Thanos is around"
+        duration="2hrs"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--BmgguRnX--/v1521804402/thor.jpg"
+        title="Thor : Ragnarok"
+        description="Thor lost his hair"
+        duration="2hrs30mins"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--qXaW5V3E--/v1521804426/wakanda.jpg"
+        title="Black Panther"
+        description="Wakanda Forever"
+        duration="2hrs15mins"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--4NgMf3RF--/v1521804358/avengers.jpg "
+        title="Avengers : Infinity War"
+        description="Thanos is around"
+        duration="2hrs"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--BmgguRnX--/v1521804402/thor.jpg"
+        title="Thor : Ragnarok"
+        description="Thor lost his hair"
+        duration="2hrs30mins"
+      />
+    </swiper-slide>
+    <swiper-slide style="width: 202px">
+      <BannerListItem
+        image="http://res.cloudinary.com/og-tech/image/upload/s--qXaW5V3E--/v1521804426/wakanda.jpg"
+        title="Black Panther"
+        description="Wakanda Forever"
+        duration="2hrs15mins"
+      />
+    </swiper-slide>
+    <div class="swiper-scrollbar" slot="scrollbar"></div>
+    <div class="swiper-button-prev" slot="button-prev"></div>
+    <div class="swiper-button-next" slot="button-next"></div>
+  </swiper>
 </template>
 
-
 <script>
+import BannerListItem from "./BannerListItem";
+import { Scrollbar, Mousewheel, Navigation } from "swiper";
+import { SwiperCore, Swiper, SwiperSlide } from "swiper-vue2";
+
+import "swiper/swiper-bundle.css";
+
+SwiperCore.use([Scrollbar, Mousewheel, Navigation]);
+
 
 export default {
-    name:'BannerList'
+    name:'BannerList',
+    components: {
+    BannerListItem,
+    Swiper,
+    SwiperSlide,
+  },
+    data() {
+    return {
+      swiperOptions: {
+        slidesPerView: "auto",
+        spaceBetween: 0,
+        freeMode: true,
+        scrollbar: true,
+        mousewheel: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+    };
+  },
 }
 </script>
 
-<style>
-
+<style scoped>
+.swiper-slide {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 202px;
+}
+.swiper-container {
+  height: 450px;
+  width: 100%;
+}
 </style>
--- final-pjt-front/src/components/BannerListItem.vue
+++ final-pjt-front/src/components/BannerListItem.vue
@@ -1,15 +1,55 @@
 <template>
-  <div>
-
+  <div class="movie" v-bind:style="{ backgroundImage: 'url(' + image + ')' }">
+    <div class="content">
+      <h2 class="title">{{ title }}</h2>
+      <p class="description">{{ description }}</p>
+      <p class="duration">{{ duration }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name:'BannerListItem'
+    name:'BannerListItem',
+    props : [
+    'image',
+    'title',
+    'description',
+    'duration'
+  ]
 }
 </script>
 
-<style>
-
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.movie{
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-end;
+  text-align: left;
+  width : 100%;
+  height : 800px;
+  /* background-color: rgba(255, 255, 255, 0.7); */
+  background-repeat: no-repeat;
+  background-blend-mode: overlay;
+  background-size: contain;
+  background-position: center;
+}
+.content {
+  padding: 10px;
+}
 </style>
