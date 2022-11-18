2495bfabc9b9f65a3a9f35e52d29080f036414d9
Lucas0105 <dnjswo105@gmail.com>
2022-11-18 10:14:31 +0900

    [add]: cors allowed localhost

--- final-pjt-back/server/settings.py
+++ final-pjt-back/server/settings.py
@@ -98,11 +98,17 @@ SPECTACULAR_SETTINGS = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
 ]
 
 ROOT_URLCONF = 'server.urls'
