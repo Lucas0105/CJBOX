af0bd781b02a6a802325ad55f02870386573ba00
cygyeong <cyellowgreen@gmail.com>
2022-11-18 09:38:59 +0900

    nav design update

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -4,27 +4,22 @@
     <nav class="navbar navbar-expand-lg" >
       <div class="container-fluid">
         <router-link class="navbar-brand" :to="{name:'movies'}"><img src="@/assets/CJBOX_logo2-2.gif" width="60px" alt=""></router-link>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav ">
-            <b-nav-item-dropdown text="장르" right  >
+          <ul class="navbar-nav">
+            <b-nav-item-dropdown class="mx-2" text="장르">
               <b-dropdown-item v-for="(genre, index) in genres" :key="index"><router-link :to="{ name: 'genre', params: {category: genre} }" >{{ genre }}</router-link></b-dropdown-item>
             </b-nav-item-dropdown>
             <div class="d-flex">
-              <li class="nav-item">
-                <router-link class="nav-link" :to="{ name: 'search'}"><b-icon icon="search"></b-icon></router-link>
+              <li class="nav-item mx-1">
+                <router-link class="nav-link" :to="{ name: 'search'}"><b-icon icon="search" title="검색"></b-icon></router-link>
               </li>
-              <li class="nav-item">
-                <router-link class="nav-link" :to="{name:'login'}"><b-icon icon="box-arrow-in-right"></b-icon></router-link>
+              <li class="nav-item mx-1">
+                <router-link class="nav-link" :to="{name:'login'}"><b-icon icon="box-arrow-in-right" title="로그인"></b-icon></router-link>
               </li>
-              <li class="nav-item">
-                <router-link class="nav-link" :to="{name:'signup'}"><b-icon icon="person-plus"></b-icon></router-link>
+              <li class="nav-item mx-1">
+                <router-link class="nav-link" :to="{name:'signup'}"><b-icon icon="person-plus" title="회원가입"></b-icon></router-link>
               </li>
             </div>
           </ul>
-        </div>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
       </div>
     </nav>
     <router-view></router-view>
@@ -54,9 +49,6 @@ export default {
 .navbar-brand{
   padding-top: 0 !important;
   padding-bottom: 0 !important
-}
-p{
-  color:#EEEEEE 
 }
 
 </style>
