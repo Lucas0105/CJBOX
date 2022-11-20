c90ecd613226787cca2add234229abcbc5e047e6
Lucas0105 <dnjswo105@gmail.com>
2022-11-20 15:01:53 +0900

    [fix]: 데이터가 없는 장르 선택시 404 에러 발생하는 문제 해결

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -42,6 +42,6 @@ def detail(request, id):
 @api_view(['GET'])
 def genre(request, genre, page):
     genre = get_object_or_404(Genre, name=genre)
-    movies = get_list_or_404(Movie, genres=genre)[(page-1) * 9:9*page]
+    movies = Movie.objects.filter(genres=genre).all()[(page-1) * 9:9*page]
     serializer = MovieListSerializer(movies, many=True)
     return Response(serializer.data)
