914a52f99ecd036a7c3f8c83bc50c03b19abd60b
Lucas0105 <dnjswo105@gmail.com>
2022-11-21 15:44:45 +0900

    [fix]: 장르 id 대신 name으로 response

--- final-pjt-back/movies/serializers.py
+++ final-pjt-back/movies/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Movie
+from .models import Movie, Genre
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ('name', 'id')
+
 
 class MovieListSerializer(serializers.ModelSerializer):
+
+    genres = GenreSerializer(many=True, read_only = True)
 
     class Meta:
         model = Movie
