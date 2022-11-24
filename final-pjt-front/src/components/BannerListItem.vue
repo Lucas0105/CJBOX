9688a7ad861f14274989f031294392a7044795b9
Lucas0105 <dnjswo105@gmail.com>
2022-11-24 15:08:07 +0900

    [add]: loading gif

--- final-pjt-front/src/components/BannerListItem.vue
+++ final-pjt-front/src/components/BannerListItem.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="movie" :title="title" :style="{ backgroundImage: 'url(' + image + ')' }" @click="toUrl">
+<div>
+  <div v-if="isLoading" class="movie empty" :title="title" @click="toUrl">
     <div class="content" >
     </div>
   </div>
+  <div v-else class="movie" :title="title" :style="{ backgroundImage: 'url(' + image + ')' }" @click="toUrl">
+    <div class="content" >
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
     name:'BannerListItem',
+    data(){
+      return {
+        isLoading: true,
+      }
+    },
     props : [
     'image',
     'id',
@@ -17,8 +28,14 @@ export default {
     toUrl() {
       this.$router.push({name:'detail', params:{id: this.id}})
       this.$router.go(this.$router.currentRoute)
+    },
+    changLoading(){
+      this.isLoading = false
     }
   },
+  mounted(){
+    setTimeout(() => {this.changLoading()}, 1500);
+  }
 }
 </script>
 
@@ -52,6 +69,9 @@ a {
   background-position: center;
   transition: all 0.2s linear;
   cursor: pointer;
+}
+.empty{
+  background-image: url('@/assets/CJBOX_logo2-1.gif');
 }
 .movie:hover{
   transform: scale(1.2);
